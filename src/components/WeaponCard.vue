c4e9cfb646e4ff5423d0db48d44cc20d46d07f50
zhanghaining0808 <2535708702@qq.com>
2025-08-13 14:56:47 +0800

    fix 修改未导入的getType 待测试和改进

--- src/components/WeaponCard.vue
+++ src/components/WeaponCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex space-x-35 px-4 py-6">
-        <div class="overflow-hidden h-[200px] shadow-2xl bg-black/30 relative text-white w-[500px] rounded-2xl">
+        <div class="overflow-hidden h-[220px] shadow-2xl bg-black/30 relative text-white w-[600px] rounded-2xl">
             <img class="w-full transition ease-in-out duration-300 hover:scale-110 h-full rounded-2xl object-cover object-bottom"
                 :src="`/weapon-${$props.name}.png`" alt="">
             <div
@@ -8,13 +8,20 @@
                 <div class="text-[25px]">
                     {{ props.name }}
                 </div>
+                <div class="text-gray-400">
+                    {{ getType(props.type) }}
+                </div>
+                <div class="flex justify-end items-center">
+                    <button
+                        class="cursor-context-menu mr-3 px-4 py-1 rounded-2xl bg-gray-700/70 hover:bg-gray-600">More</button>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
-
+// TODO 待测试 待改进
 const props = defineProps(["name", "type"])
 console.log(props.name, props.type)
 
